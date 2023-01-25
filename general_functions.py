b48f7cb0df3d3e1595b174a85e798ae62b086e54
pismosv <sv.kirill.a@gmail.com>
2023-01-25 19:47:24 +0300

    dev 0.6

--- general_functions.py
+++ general_functions.py
@@ -3,8 +3,14 @@ import sys
 import pygame
 
 
+pygame.init()
+pygame.key.set_repeat(200, 200)
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+
+
 def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
+    fullname = os.path.join('data\images', name)
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
